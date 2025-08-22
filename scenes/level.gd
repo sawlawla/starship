54f45a95f47b12449efb7c42b07b7699a827f801
sawlawla <lawlasaw619@gmail.com>
2025-08-22 18:40:50 +0630

    mysecondcommit

--- scenes/level.gd
+++ scenes/level.gd
@@ -3,6 +3,16 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://laser.tscn")
 
+func _ready():
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		var random_x = rng.randi_range(0, size.x)
+		var random_y = rng.randi_range(0, size.y)
+		star.position = Vector2(random_x,random_y)
+		
+		var random_scale = rng.randf_range(1,2)
+		star.scale = Vector2(2,1)
 func _on_meteor_timer_timeout() -> void:
 	
 	var metor = meteor_scene.instantiate()
